3603ddf4234e7dda309af7d190ad84f09d741d63
olegkrasilev <olegkrasilev@gmail.com>
2022-01-17 15:26:38 +0300

    fix: updates eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return <h1>Hello world</h1>;
-}
-
-export default App;
+export const App: React.FC = () => <h1>Hello world</h1>;
