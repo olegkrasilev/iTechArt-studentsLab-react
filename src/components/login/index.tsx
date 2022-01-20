6822c4fabf8a02397b05a78c08023f117a5f895a
olegkrasilev <olegkrasilev@gmail.com>
2022-01-20 11:57:18 +0300

    feature(login) : userForgotPassword modal.

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,51 +1,66 @@
 import React, { useState } from 'react';
-
 import { Button, TextField } from '@mui/material';
 
 import { Wrapper } from 'src/components/login/style';
+import { ForgotPasswordModal } from 'src/components/login/forgoPasswordModal';
 
 export const Login = () => {
-  const [isUserForgotPassword, setIsUserForgotPassword] = useState(false);
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [isForgotUserPasswordModalOpen, setForgotUserPasswordModalOpen] = React.useState(false);
 
   const toggleUserForgotPassword = () => {
-    setIsUserForgotPassword(!isUserForgotPassword);
+    setIsUserLoggedIn(!isUserLoggedIn);
+  };
+
+  const openIsUserForgotPasswordModal = () => {
+    setForgotUserPasswordModalOpen(true);
+  };
+
+  const closeIsUserForgotPasswordModal = () => {
+    setForgotUserPasswordModalOpen(false);
   };
 
   return (
-    <Wrapper>
-      <div className="form__left" />
-      <form className="form__right" noValidate autoComplete="false">
-        <div className="wrapper__inner">
-          <h1 className="title">
-            {isUserForgotPassword ? (
-              <span className="title__welcome">
-                Welcome back <span>Login to your account</span>
-              </span>
-            ) : (
-              <span className="title__registration">Registration</span>
+    <>
+      <Wrapper>
+        <div className="form__left" />
+        <form className="form__right" noValidate autoComplete="false">
+          <div className="wrapper__inner">
+            <h1 className="title">
+              {isUserLoggedIn ? (
+                <span className="title__welcome">
+                  Welcome back <span>Login to your account</span>
+                </span>
+              ) : (
+                <span className="title__registration">Registration</span>
+              )}
+            </h1>
+            <TextField className="input" type="email" label="email" variant="outlined" required />
+            <TextField className="input" type="password" label="password" variant="outlined" required />
+            {isUserLoggedIn && (
+              <TextField className="input" type="text" label="First Name" variant="outlined" required />
             )}
-          </h1>
-          <TextField className="input" type="email" label="email" variant="outlined" required />
-          <TextField className="input" type="password" label="password" variant="outlined" required />
-          {isUserForgotPassword && (
-            <TextField className="input" type="text" label="First Name" variant="outlined" required />
-          )}
-          {isUserForgotPassword && (
-            <TextField className="input" type="text" label="Last Name" variant="outlined" required />
-          )}
-          <Button className="loginBtn button" variant="contained" color="success" size="large">
-            {isUserForgotPassword ? 'Login now' : 'Register'}
-          </Button>
-          {isUserForgotPassword && (
-            <Button className="button" variant="text">
-              Forgot password?
+            {isUserLoggedIn && (
+              <TextField className="input" type="text" label="Last Name" variant="outlined" required />
+            )}
+            <Button className="loginBtn button" variant="contained" color="success" size="large">
+              {isUserLoggedIn ? 'Login now' : 'Register'}
             </Button>
-          )}
-          <Button onClick={toggleUserForgotPassword} className="button" variant="text">
-            {isUserForgotPassword ? 'Sign up' : 'Already have an account?'}
-          </Button>
-        </div>
-      </form>
-    </Wrapper>
+            {isUserLoggedIn && (
+              <Button onClick={openIsUserForgotPasswordModal} className="button" variant="text">
+                Forgot password?
+              </Button>
+            )}
+            <Button onClick={toggleUserForgotPassword} className="button" variant="text">
+              {isUserLoggedIn ? 'Sign up' : 'Already have an account?'}
+            </Button>
+          </div>
+        </form>
+      </Wrapper>
+      <ForgotPasswordModal
+        closeIsUserForgotPasswordModal={closeIsUserForgotPasswordModal}
+        isForgotUserPasswordPasswordModalOpen={isForgotUserPasswordModalOpen}
+      />
+    </>
   );
 };
