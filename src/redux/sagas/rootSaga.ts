35f7c83907e04934cecdc1a187d68c5d1789be3c
olegkrasilev <olegkrasilev@gmail.com>
2022-01-25 15:46:32 +0300

    fix(rootSaga): fix imports

--- src/redux/sagas/rootSaga.ts
+++ src/redux/sagas/rootSaga.ts
@@ -1,14 +1,15 @@
 import { put, takeEvery } from 'redux-saga/effects';
 
-import { login } from '../../API/login';
-import { loginEndpoint } from '../../API/endpoints';
-import { UserActions } from '../../types/user';
+import { login } from 'src/API/login';
+import { loginEndpoint } from 'src/API/endpoints';
+import { UserActions } from 'src/types/user';
 
 export function* watchFetchUser(payload: { payload: { email: string; password: string }; type: string }) {
   const { email, password } = payload.payload;
   try {
     const data: { id: string; firstName: string; lastName: string; accessToken: string; refreshToken: string } =
       yield login(loginEndpoint, { email, password });
+
     const { id, firstName, lastName, accessToken, refreshToken } = data;
     const dataToStore = {
       userID: id,
