b5768c1026c8686c535a0d368aa7e91472d17e76
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2022-01-26 16:21:01 +0300

    fix(login) : example code

--- src/redux/sagas/rootSaga.ts
+++ src/redux/sagas/rootSaga.ts
@@ -1,4 +1,4 @@
-import { put, takeEvery } from 'redux-saga/effects';
+import { call, put, takeEvery } from 'redux-saga/effects';
 
 import { login } from 'src/API/login';
 import { loginEndpoint } from 'src/API/endpoints';
@@ -15,27 +15,44 @@ export interface ResponseGenerator {
   ok?: boolean;
 }
 
+async function getPeople() {
+  const request = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+
+  return request.json();
+}
+
 export function* watchFetchUser(payload: { payload: { email: string; password: string }; type: string }) {
-  const { email, password } = payload.payload;
   try {
-    const response: ResponseGenerator = yield login(loginEndpoint, { email, password });
+    const data: ReturnType<typeof getPeople> = yield call(getPeople);
 
-    if (!response.ok) {
-      throw new Error('Something went wrong... Please try again');
-    }
+    console.log(data);
 
-    const userData = response as Promise<globalThis.Response>;
-
-    userData.then(result => console.log(result)).catch();
-
-    yield put({ type: UserActions.fetchUserSuccess, payload });
+    yield put({ type: UserActions.fetchUserSuccess, payload: data });
   } catch (error) {
     if (error instanceof Error) {
       const errorMessage = error.message;
 
+      console.log(errorMessage);
+
       yield put({ type: UserActions.fetchUserError, payload: errorMessage });
     }
   }
+
+  // const { email, password } = payload.payload;
+  // try {
+  //   const response: ResponseGenerator = yield login(loginEndpoint, { email, password });
+  //   if (!response.ok) {
+  //     throw new Error('Something went wrong... Please try again');
+  //   }
+  //   const userData = response as Promise<globalThis.Response>;
+  //   userData.then(result => console.log(result)).catch();
+  //   yield put({ type: UserActions.fetchUserSuccess, payload });
+  // } catch (error) {
+  //   if (error instanceof Error) {
+  //     const errorMessage = error.message;
+  //     yield put({ type: UserActions.fetchUserError, payload: errorMessage });
+  //   }
+  // }
 }
 
 export function* rootSaga() {
