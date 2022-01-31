18b5c12471c6505db92e4965d59d7c57b203e3fd
olegkrasilev <olegkrasilev@gmail.com>
2022-01-31 10:23:39 +0300

    feat(app): npm i date-fns

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,11 +1,12 @@
 import Cookies from 'js-cookie';
+import { addMinutes, addDays } from 'date-fns';
 
 export const getAccessJwtToken = () => Cookies.get('jwtAccessToken');
 
 export const setAccessJwtToken = async (jwtAccessToken: string) => {
   // TODO add env file
   Cookies.set('jwtAccessToken', jwtAccessToken, {
-    expires: 7,
+    expires: addMinutes(new Date(), 10),
     sameSite: 'Strict',
     secure: true,
   });
@@ -15,7 +16,7 @@ export const getRefreshToken = () => Cookies.get('jwtRefreshToken');
 
 export const setRefreshToken = (jwtRefreshToken: string) => {
   Cookies.set('jwtRefreshToken', jwtRefreshToken, {
-    expires: 7,
+    expires: addDays(new Date(), 30),
     sameSite: 'Strict',
     secure: true,
   });
