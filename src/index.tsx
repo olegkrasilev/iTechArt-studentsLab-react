b6b6e43abc0703f1b25d724b2f7bd98a743913c7
olegkrasilev <olegkrasilev@gmail.com>
2022-02-07 13:55:59 +0300

    feat(app): error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 
+import Error from 'src/pages/error';
 import { store } from 'src/redux/store';
 import { App } from 'src/App';
 import { GlobalStyles } from 'src/styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <GlobalStyles />
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ErrorBoundary FallbackComponent={Error}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <GlobalStyles />
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.querySelector('#root'),
 );
