385ec7dc4e09fa81cc40d7059c4784b372685abd
olegkrasilev <olegkrasilev@gmail.com>
2022-02-07 13:50:16 +0300

    feat(app): react-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import { Navigation } from 'src/routes';
 
-export const App: React.FC = () => <Navigation />;
+export const App: React.FC = () => (
+  <>
+    <Helmet>
+      <title>iTechArt Students Lab </title>
+      <meta name="description" content="pet project" />
+    </Helmet>
+    <Navigation />
+  </>
+);
