2baa30777b587fda251ace9c99a8f4c258cc14ff
olegkrasilev <olegkrasilev@gmail.com>
2022-02-09 12:12:35 +0300

    fix(posts-saga): types

--- src/redux/sagas/posts/index.ts
+++ src/redux/sagas/posts/index.ts
@@ -1,6 +1,5 @@
 import { call, put, takeEvery, all } from 'redux-saga/effects';
 import axios from 'axios';
-import { TakeableChannel } from 'redux-saga';
 
 import { allPostsEndpoint, editPostEndpoint, requestPostEndpoint } from 'src/constants/endpoints';
 import { Post, PostsActions, EditPostActionType, RequestPostActionType } from 'src/types/posts';
@@ -30,7 +29,7 @@ export function* loadAllPosts() {
   }
 }
 
-export function* editPost(payload: { payload: { postID: number; post: string | null; title: string } }) {
+export function* editPost(payload: { payload: { postID: number; post: string | null; title: string }; type: string }) {
   const { post, postID, title } = payload.payload;
   try {
     yield call(axios.patch, editPostEndpoint, { postID, post, title }, { withCredentials: true });
@@ -44,7 +43,7 @@ export function* editPost(payload: { payload: { postID: number; post: string | n
   }
 }
 
-export function* requestPost(payload: { postID: string }) {
+export function* requestPost(payload: { postID: string; type: string }) {
   const { postID } = payload;
   try {
     const response: {
@@ -74,11 +73,11 @@ export function* watchLoadPosts() {
 }
 
 export function* watchEditPost() {
-  yield takeEvery(EditPostActionType.pending as unknown as TakeableChannel<unknown>, editPost);
+  yield takeEvery(EditPostActionType.pending, editPost);
 }
 
 export function* watchRequestPost() {
-  yield takeEvery(RequestPostActionType.pending as unknown as TakeableChannel<unknown>, requestPost);
+  yield takeEvery(RequestPostActionType.pending, requestPost);
 }
 
 export function* postsSaga() {
