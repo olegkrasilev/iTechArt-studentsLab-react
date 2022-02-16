3eaa83937d95116cbc90eaf4115fa65f4647372e
olegkrasilev <olegkrasilev@gmail.com>
2022-02-16 14:56:26 +0300

    fix(allUsers): logic,styles,types

--- src/pages/allUsers/index.tsx
+++ src/pages/allUsers/index.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Backdrop, CircularProgress, Container, Grid, Pagination } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
-import { makeStyles } from '@mui/styles';
 import { useNavigate, useParams } from 'react-router-dom';
 
+import { useStyles } from './style';
 import User from './user';
 
 import { loadUsersAction } from 'src/redux/action/users';
@@ -24,35 +24,12 @@ const AllUsers: React.FC = () => {
     dispatch(loadUsersAction(page));
   }, [dispatch, page]);
 
-  const useStyles = makeStyles({
-    centerPagination: {
-      display: 'flex',
-      justifyContent: 'center',
-      marginTop: 20,
-      marginBottom: 20,
-    },
-    title: {
-      textAlign: 'center',
-      fontSize: 40,
-    },
-  });
-
   const classes = useStyles();
 
-  const handlePaginationClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const paginationButtonNumber = (event.target as HTMLButtonElement).textContent;
-
-    if (paginationButtonNumber) {
-      navigate(`../allUsers/${paginationButtonNumber}`, { replace: true });
-      setDefaultPage(+paginationButtonNumber);
-    }
+  const handlePaginationClick = (vent: React.ChangeEvent<unknown>, paginationPage: number) => {
+    navigate(`../allUsers/${paginationPage}`, { replace: true });
+    setDefaultPage(paginationPage);
   };
-
-  const renderUsers = allUsers.map(item => {
-    const { email, firstName, lastName, id } = item;
-
-    return <User key={id} email={email} firstName={firstName} lastName={lastName} />;
-  });
 
   if (isAllUsersLoading) {
     return (
@@ -64,22 +41,26 @@ const AllUsers: React.FC = () => {
 
   return (
     <Container>
-      {allUsers.length === 0 ? (
-        <h1 className={classes.title}>No Users were found</h1>
-      ) : (
+      {allUsers.length > 0 ? (
         <ul>
           <Grid container spacing={2}>
-            {renderUsers}
+            {allUsers.map(item => {
+              const { email, firstName, lastName, id } = item;
+
+              return <User key={id} email={email} firstName={firstName} lastName={lastName} />;
+            })}
           </Grid>
           <Pagination
             hidePrevButton
             hideNextButton
             className={classes.centerPagination}
             page={defaultPage}
-            onClick={handlePaginationClick}
+            onChange={handlePaginationClick}
             count={paginationButtonsCount}
           />
         </ul>
+      ) : (
+        <h1 className={classes.title}>No Users were found</h1>
       )}
     </Container>
   );
