62e04792c7f714f522ed6522f0b5e6703f1f9222
olegkrasilev <olegkrasilev@gmail.com>
2022-02-17 14:28:27 +0300

    fix(login): split into a presentation/container

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { Backdrop, CircularProgress } from '@mui/material';
 
 import { selectIsUserAuthorized } from 'src/redux/selector';
-import { Login } from 'src/pages/login/index';
+import Login from 'src/pages/login/container';
 import { NotFound } from 'src/pages/notFound';
 import { Layout } from 'src/pages/layout';
 
