f59e85373e50d8e7f9071aef2b6e38693eef0934
olegkrasilev <olegkrasilev@gmail.com>
2022-02-23 11:31:20 +0300

    fix(app.test): format file

--- src/__test__/app.test.jsx
+++ src/__test__/app.test.jsx
@@ -1,10 +1,7 @@
 import App from 'src/App';
 
 describe('on initial render should be in the dom', () => {
-
-
-    it('Should be in the dom', () => {
-
-        expect(App).toBeInTheDocument;
-    });
+  it('Should be in the dom', () => {
+    expect(App).toBeInTheDocument;
+  });
 });
