c834333379ff0f740193cfc95fa32b3aa6eb65fb
olegkrasilev <olegkrasilev@gmail.com>
2022-03-04 12:04:29 +0300

    fix(account): styles

--- src/pages/account/style.tsx
+++ src/pages/account/style.tsx
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 export const StyledDiv = styled.div`
   display: grid;
   grid-template-columns: 0.5fr 1fr;
-  align-items: center;
+  align-items: start;
 `;
